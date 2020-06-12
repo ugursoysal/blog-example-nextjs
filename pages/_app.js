f5f8d3a15c1de88fd2f7c3e42b5d92f02a9d4727
wheeze <ugursysl@gmail.com>
2020-06-13 02:21:33 +0300

    colors and images added/changed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ApolloProvider } from '@apollo/react-hooks'
 import { useApollo } from '../lib/apolloClient'
 
-export default function Layout({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState)
   return (
     <ApolloProvider client={apolloClient}>
